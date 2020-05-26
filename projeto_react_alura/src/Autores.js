ca090c9356d271834250cade6e0a5c7e9d0ae5e9
Ricardo Mogio <ricardo.mogio@gmail.com>
2020-05-26 16:08:19 -0300

    Alterado arquivos de Autores e Livros

--- projeto_react_alura/src/Autores.js
+++ projeto_react_alura/src/Autores.js
@@ -1,13 +1,54 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import Header from './Header'
+import DataTable from './DataTable'
 
-const Autores = () => {
-  return (
-    <Fragment>
-      <Header/>
-      <h1>Autores</h1>
-    </Fragment>
-  )
+
+class Autores extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state={
+      autores: [
+        {
+          nome: 'Ricardo',
+          livro: 'Sei la',
+          preco: '1'
+        },
+    
+        {
+          nome: 'Gabi',
+          livro: 'Sei la2',
+          preco: '2'
+        },
+    
+        {
+          nome: 'Amy',
+          livro: 'Roubando',
+          preco: '3'
+        },
+    
+        {
+          nome: 'Luna',
+          livro: 'Petisco',
+          preco: '4'
+        }
+      ],
+      titulo: 'Autores'
+    }
+  }
+
+  render(){
+    return (
+      <Fragment>
+        <Header/>
+        <div className="container">
+          <h1>Página de Autores</h1>
+          <DataTable dados={this.state.autores} titulo={this.state.titulo} colunas={['nome']}/>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default Autores
--- projeto_react_alura/src/Livros.js
+++ projeto_react_alura/src/Livros.js
@@ -1,13 +1,54 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import Header from './Header'
+import DataTable from './DataTable'
 
-const Livros = () => {
-  return (
-    <Fragment>
-      <Header/>
-      <h1>Livros</h1>
-    </Fragment>
-  )
+
+class Livros extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state={
+      livros: [
+        {
+          nome: 'Ricardo',
+          livro: 'Sei la',
+          preco: '1'
+        },
+    
+        {
+          nome: 'Gabi',
+          livro: 'Sei la2',
+          preco: '2'
+        },
+    
+        {
+          nome: 'Amy',
+          livro: 'Roubando',
+          preco: '3'
+        },
+    
+        {
+          nome: 'Luna',
+          livro: 'Petisco',
+          preco: '4'
+        }
+      ],
+      titulo: 'Livros'
+    }
+  }
+
+  render(){
+    return (
+      <Fragment>
+        <Header/>
+        <div className="container">
+          <h1>Página de Livros</h1>
+          <DataTable dados={this.state.livros} titulo={this.state.titulo} colunas={['livro']}/>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default Livros
